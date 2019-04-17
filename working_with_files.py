28be88ecd41167c8c92f55a731194b42300c7cb2
Carlos Javier Oliva Domínguez <carlos-javier.olivadominguez.external@atos.net>
2019-04-17 08:47:36 +0100

    Se logra coger los ficheros y moverlos a carpetas segun su nombre.

--- working_with_files.py
+++ working_with_files.py
@@ -3,12 +3,16 @@
 import os
 import shutil
 
-# Function to rename multiple files
-
 # Ruta actual de los ficheros de entrada
-allfiles = os.listdir('C:\\Users\\A704806\\Python\\mytest')
-filetype = 'Estado_2'
+os.chdir('C:\\Users\\A704806\\Desktop\\files')
+srcpath = ('C:\\Users\\A704806\\Documents\\SOA\\MONITORIZACION\\20190414_ficheros_salida.tar\\TRAN\\2')
 
-for file in allfiles:
-    if file.startswith(filetype):
-        print(file)
+
+
+for file in os.listdir(srcpath):
+    foldername = file[11:-25]
+    if not os.path.exists(foldername):
+        os.mkdir(foldername)
+        shutil.copy(os.path.join(srcpath, file), foldername)
+    else:
+        shutil.copy(os.path.join(srcpath, file), foldername)
