869c0998b92eb83f2db3b261772f72a4cc240b8b
Carlos Javier Oliva Domínguez <carlos-javier.olivadominguez.external@atos.net>
2019-04-22 15:08:51 +0100

    Se modifica la herramienta donde se hace un split del nombre el fichero para coger el tópico y crear la correspondiente carpeta. Luego se mueven los ficheros a la carpeta correspondiente. Se mantienen rutas estáticas

--- working_with_files.py
+++ working_with_files.py
@@ -1,4 +1,6 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
+# @author Carlos
+# v1.1
 # importing  modules
 import os
 import shutil
@@ -7,39 +9,43 @@ import shutil
 os.chdir('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar')
 srcpath = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\coger')
 dest = ('C:\\Users\\A704806\\Desktop\\files\\')
-os.mkdir('AMAZON')
-amazon = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\AMAZON')
-os.mkdir('TRAN')
-tran = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\TRAN')
-os.mkdir('UNICO')
-unico = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\UNICO')
-os.mkdir('ICP_WS')
-icp = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\ICP_WS')
-os.mkdir('OPENBANK')
-openbank = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\OPENBANK')
-os.mkdir('EMSEVT')
-emsevt = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\EMSEVT')
-os.mkdir('EVT')
-evt = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\EVT')
-os.mkdir('INTERBOX')
-interbox = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\INTERBOX')
 
+#Comprobamos los tópicos en los archivos y creamos las correspondientes carpetas
+for f in os.listdir(srcpath):
+    splitname = f.split('_')
+    foldername = splitname[2]
+    if not os.path.exists(foldername):
+        os.mkdir(foldername)
+
+
+
+# #COPIAMOS los ficheros a su carpeta correspondiente
 for filename in os.listdir(srcpath):
     root, ext = os.path.splitext(filename)
-    if '_AMAZON_' in filename:
-        shutil.move(os.path.join(srcpath, filename), amazon)
-    elif '_TRAN' in filename:
-        shutil.move(os.path.join(srcpath, filename), tran)
-    elif '_ICP_WS_' in filename:
-        shutil.move(os.path.join(srcpath, filename), icp)
-    elif 'UNICO' in filename:
-        shutil.move(os.path.join(srcpath, filename), unico)
-    elif 'OPENBANK' in filename:
-        shutil.move(os.path.join(srcpath, filename), openbank)
-    elif 'EMSEVT' in filename:
-        shutil.move(os.path.join(srcpath, filename), emsevt)
-    elif 'EVT' in filename:
-        shutil.move(os.path.join(srcpath, filename), evt)
+    if 'AMAZON' in filename:
+        amazon = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\AMAZON')
+        shutil.copy(os.path.join(srcpath, filename), amazon)
     elif 'INTERBOX' in filename:
-        shutil.move(os.path.join(srcpath, filename), interbox)
-
+        interbox = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\INTERBOX')
+        shutil.copy(os.path.join(srcpath, filename), interbox)
+    elif 'TRAN' in filename:
+        tran = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\TRAN')
+        shutil.copy(os.path.join(srcpath, filename), tran)
+    elif 'UNICO' in filename:
+        unico = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\UNICO')
+        shutil.copy(os.path.join(srcpath, filename), unico)
+    elif 'ICP_WS' in filename:
+        icp = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\ICP')
+        shutil.copy(os.path.join(srcpath, filename), icp)
+    elif 'OPENBANK' in filename:
+        openbank = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\OPENBANK')
+        shutil.copy(os.path.join(srcpath, filename), openbank)
+    elif 'EMSEVT' in filename:
+        emsevt = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\EMSEVT')
+        shutil.copy(os.path.join(srcpath, filename), emsevt)
+    elif 'EVT' in filename:
+        evt = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\EVT')
+        shutil.copy(os.path.join(srcpath, filename), evt)
+    elif 'VDFNE' in filename :
+        vdfne = ('C:\\Users\\A704806\\Desktop\\MONITORIZACION\\dejar\\VDFNE')
+        shutil.copy(os.path.join(srcpath, filename), vdfne)
