5b2398afa7663fef5876bb967045c156f610206a
Carlos Javier Oliva Domínguez <carlos-javier.olivadominguez.external@atos.net>
2019-04-24 12:13:44 +0100

    Se añade una mayor interacción a la herramienta.

--- working_with_files.py
+++ working_with_files.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 # @author Carlos
-# v1.1
+# v2.0
 # importing  modules
 import os
 import shutil
+import sys
 
 # path for input and output files
 srcpath = input('Por favor indique la ruta de los ficheros de entrada: ')
 dest = input('Por favor indique la ruta para dejar los ficheros: ')
+
+if srcpath:
+    print('La ruta de entrada es: ' + srcpath)
+else:
+    sys.exit('¡Debe indicar una de entrada! Finalizando programa...')
+if dest:
+     print('La ruta de salida es: ' + dest)
+else:
+    sys.exit('¡Debe indicar una ruta de salida! Finalizando programa...')
+print('Clasificando ficheros...')
 os.chdir(dest)
 
 #Create folders by file name, and then move the files to the corresponding folder
@@ -17,3 +28,4 @@ for f in os.listdir(srcpath):
     if not os.path.exists(foldername):
         os.mkdir(foldername)
     shutil.move(os.path.join(srcpath, f), foldername)
+print('¡Finalizado!')
