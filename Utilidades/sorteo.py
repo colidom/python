c815fb8a15507df49179cd5b1363b86eec55375c
Carlos Oliva <carlos.oliva@correos.com>
2020-01-16 16:18:34 +0000

    Pedimos participantes por consola

--- Utilidades/sorteo.py
+++ Utilidades/sorteo.py
@@ -1,16 +1,22 @@
 import random
 
-users = ["Carlos","Pepe","Luis","María","Alberto", "Marta"]
+users = []
 
 try:
   while True:
     print("")
     print("___Lista de opciones___")
-    print("1. Roll")
-    print("2. Exit")
+    print("1. Añadir participante")
+    print("2. Roll")
+    print("3. Exit")
     option = input("Por favor elija una opción: ")
     print(option)
-    if option == "1":
+
+    if  option == "1":
+      nombre = input("Introduzca el nombre del participante: ")
+      users.append(nombre)
+      print(f"Hay {users} usuarios")
+    elif option == "2":
         random.shuffle(users)
         agraciado = users.pop(1)
         print(f"Opcion elegida: {option}")
