cc2079c0a17406b5f3f352aabf775fb4527afa4b
colidom <carlos.oliva.dom@gmail.com>
2020-01-18 21:23:04 +0000

    Añadido try except para lanzar el menú nuevamente en caso de que no se introduzca ninguna opción

--- Utilidades/agenda.py
+++ Utilidades/agenda.py
@@ -18,30 +18,33 @@ def remove_contact(phone_book, name):
 
 def menu():
     phone_book = {}
-    while True:
-        print("1. Mostrar lista de contactos.")
-        print("2. Insertar un nuevo contacto.")
-        print("3. Borrar un contacto.")
-        print("4. Salir. ")
-        print("******************************")
-        option = input("Elija una opción: ")
-        option = int(option)
-        if option == 1:
-            show_contacts(phone_book)
-        if option == 2:
-            name = input("Introduzca el nombre: ")
-            if name in phone_book:
-                print("Error, el Nombre introducido ya existe.")
-            else:
-                phone = input("Introduzca el número de teléfono: ")
-                add_contact(phone_book, name, phone)
-        if option == 3:
-            name = input("Nombre del contacto que decea eliminar: ")
-            if name in phone_book:
-                remove_contact(phone_book, name)
-            else:
-                print("Error... Nombre incorrecto. ")
-        if option == 4:
-            print("Adiós.")
-            break
+    try:
+        while True:
+            print("1. Mostrar lista de contactos.")
+            print("2. Insertar un nuevo contacto.")
+            print("3. Borrar un contacto.")
+            print("4. Salir. ")
+            print("******************************")
+            option = input("Elija una opción: ")
+            option = int(option)
+            if option == 1:
+                show_contacts(phone_book)
+            if option == 2:
+                name = input("Introduzca el nombre: ")
+                if name in phone_book:
+                    print("Error, el Nombre introducido ya existe.")
+                else:
+                    phone = input("Introduzca el número de teléfono: ")
+                    add_contact(phone_book, name, phone)
+            if option == 3:
+                name = input("Nombre del contacto que decea eliminar: ")
+                if name in phone_book:
+                    remove_contact(phone_book, name)
+                else:
+                    print("Error... Nombre incorrecto. ")
+            if option == 4:
+                print("Adiós.")
+                break
+    except:
+        menu()
 menu()
