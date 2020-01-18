2c84ab3ba7cfe8d3e5ffc7fc831d5a6781e75897
colidom <carlos.oliva.dom@gmail.com>
2020-01-18 21:34:18 +0000

    Añadido el portfolio

--- Flask-web/src/index.py
+++ Flask-web/src/index.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
