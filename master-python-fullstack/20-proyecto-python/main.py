4676e15dd36404524c1b04d8afd25a85577edbb2
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2020-10-16 00:27:34 +0100

    Seccion 20: Proyecto: Aplicación de consola con Python y MySQL

---
+++ master-python-fullstack/20-proyecto-python/main.py
@@ -0,0 +1,9 @@
+"""
+Proyecto Python y Mysql:
+- Abrir asistente:
+- Login o registro
+- Si elegimos registro, creará un usuario en la ddbb
+- Si elegimos login, identifica al usuario y nos preguntará
+- Crear nota, mostrar notas, borrarlas
+"""
+
