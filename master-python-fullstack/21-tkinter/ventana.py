e03269e9f18c7b9ce7fe63e8a6f1798a02c37869
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2020-10-20 00:25:37 +0100

    Modificado programa añadiendo una clase con varios métodos

--- master-python-fullstack/21-tkinter/ventana.py
+++ master-python-fullstack/21-tkinter/ventana.py
@@ -2,21 +2,56 @@
 # Módulo para crear interfaces gráficas de usuario
 
 from tkinter import *
+import os.path
 
-# Crear la ventana raiz
-ventana = Tk()
+class Programa:
+    
+    def __init__(self):
+        self.title = "Master en Python con Victor Robles"
+        self.icon = './imagenes/logo.ico'
+        self.icon_alt = './21-tkinter/imagenes/logo.ico'
+        self.size = "770x470"
+        self.resizable = False
 
-# Título de la ventana
-ventana.title("Interfaz gráfica con Python")
-# Icono de la ventana
-ventana.iconbitmap("./21-tkinter/imagenes/logo.ico")
+    def cargar(self):  
+        # Crear la ventana raiz
+        ventana = Tk()
+        self.ventana = ventana
 
-# Cambio en el tamaño de la ventana
-ventana.geometry("750x450")
+        # Título de la ventana
+        ventana.title(self.title)
 
-# Bloquear el tamaño de la ventana
-ventana.resizable(0, 0)
+        # Comprobar si existe un archivo
+        ruta_icono = os.path.abspath(self.icon)
+        if not os.path.isfile(ruta_icono):
+            ruta_icono = os.path.abspath(self.icon_alt)
+        # Icono de la ventana
+        ventana.iconbitmap(ruta_icono)
+
+        # Mostrar texto en el programa
+        texto = Label(ventana, text=ruta_icono)
+        texto.pack()
+
+        # Cambio en el tamaño de la ventana
+        ventana.geometry(self.size)
+
+        # Bloquear el tamaño de la ventana
+        if self.resizable:
+            ventana.resizable(1, 1)
+        else:
+            ventana.resizable(0, 0)
 
 
-# Arrancar y mostrar la ventana hasta que se cierre
-ventana.mainloop()
+    def addTexto(self, dato):
+        texto = Label(self.ventana, text=dato)
+        texto.pack()
+
+    def mostrar(self):
+        # Arrancar y mostrar la ventana hasta que se cierre
+        self.ventana.mainloop()
+
+# Instanciar mi programa
+programa = Programa()
+programa.cargar()
+programa.addTexto("Hola desde un método!!!")
+programa.mostrar()
