b10f584fc477813ae87d8a41a81f59c3022e723a
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2020-10-28 23:18:01 +0000

    136: Textarea o campo de texto grande

--- master-python-fullstack/21-tkinter/06-formularios.py
+++ master-python-fullstack/21-tkinter/06-formularios.py
@@ -34,4 +34,19 @@ campo_texto = Entry(ventana)
 campo_texto.grid(row=2, column=1, sticky=W, padx=5, pady=5)
 campo_texto.config(justify="left", state="normal")
 
+# Label para el campo(descripción)
+label = Label(ventana, text="Descripción: ")
+label.grid(row=3, column=0, sticky=N, padx=5, pady=5)
+
+# Campo de texto grande(descripción)
+campo_grande = Text(ventana)
+campo_grande.grid(row=3, column=1)
+campo_grande.config(
+    width=30, 
+    height=5, 
+    font=("Arial", 12),
+    padx=15,
+    pady=15
+)
+
 ventana.mainloop()
