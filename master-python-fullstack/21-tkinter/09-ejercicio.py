cee8a8abf8a393912beb7f1d5fe5db13317d71c6
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2020-10-29 19:12:36 +0000

    144: Ejercicio - Operaciones vinculadas al formulario

--- master-python-fullstack/21-tkinter/09-ejercicio.py
+++ master-python-fullstack/21-tkinter/09-ejercicio.py
@@ -5,6 +5,7 @@ CALCULADORA:
 - Mostrar el resultado en una alerta
 """
 from tkinter import *
+from tkinter import messagebox
 
 ventana = Tk()
 ventana.geometry("500x250")
@@ -12,6 +13,29 @@ ventana.title("Ejercicio completo con Tkinter | Carlos Oliva")
 ventana.config(
     bd=25
 )
+
+def sumar():
+    resultado.set(float(numero1.get()) + float(numero2.get()))
+    mostrarResultado()
+
+def restar():
+    resultado.set(float(numero1.get()) - float(numero2.get()))
+    mostrarResultado()
+
+def multiplicar():
+    resultado.set(float(numero1.get()) * float(numero2.get()))
+    mostrarResultado()
+
+def dividir():
+    resultado.set(float(numero1.get()) / float(numero2.get()))
+    mostrarResultado()
+
+def mostrarResultado():
+    messagebox.showinfo("Resultado", f"El resultado de la operación es: {resultado.get()}")
+    numero1.set("")
+    numero2.set("")
+
+
 numero1 = StringVar()
 numero2 = StringVar()
 resultado = StringVar()
@@ -35,10 +59,10 @@ Entry(marco, textvariable=numero2, justify=CENTER).pack()
 # Separador
 Label(marco, text="").pack()
 
-Button(marco, text="Sumar").pack(side="left", fill=X, expand=YES)
-Button(marco, text="Restar").pack(side="left", fill=X, expand=YES)
-Button(marco, text="Multiplicar").pack(side="left", fill=X, expand=YES)
-Button(marco, text="Dividir").pack(side="left", fill=X, expand=YES)
+Button(marco, text="Sumar", command=sumar).pack(side="left", fill=X, expand=YES)
+Button(marco, text="Restar", command=restar).pack(side="left", fill=X, expand=YES)
+Button(marco, text="Multiplicar", command=multiplicar).pack(side="left", fill=X, expand=YES)
+Button(marco, text="Dividir", command=dividir).pack(side="left", fill=X, expand=YES)
 
 
 ventana.mainloop()
