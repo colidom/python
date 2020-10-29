f32e6ea4c0fe4e6fe8d5650db8a1798a3669eeb9
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2020-10-29 19:05:43 +0000

    143. Ejercicio - Marcos

--- master-python-fullstack/21-tkinter/09-ejercicio.py
+++ master-python-fullstack/21-tkinter/09-ejercicio.py
@@ -7,7 +7,7 @@ CALCULADORA:
 from tkinter import *
 
 ventana = Tk()
-ventana.geometry("500x200")
+ventana.geometry("500x250")
 ventana.title("Ejercicio completo con Tkinter | Carlos Oliva")
 ventana.config(
     bd=25
@@ -16,19 +16,29 @@ numero1 = StringVar()
 numero2 = StringVar()
 resultado = StringVar()
 
-Label(ventana, text="Primer número: ").pack()
-Entry(ventana, textvariable=numero1).pack()
+marco = Frame(ventana, width=300, height=250)
+marco.config(
+    padx=15,
+    pady=15,
+    bd=5,
+    relief=SOLID
+)
+marco.pack(side=TOP, anchor=CENTER)
+marco.pack_propagate(False)
 
-Label(ventana, text="Segundo número: ").pack()
-Entry(ventana, textvariable=numero2).pack()
+Label(marco, text="Primer número: ").pack()
+Entry(marco, textvariable=numero1, justify=CENTER).pack()
+
+Label(marco, text="Segundo número: ").pack()
+Entry(marco, textvariable=numero2, justify=CENTER).pack()
 
 # Separador
-Label(ventana, text="").pack()
+Label(marco, text="").pack()
 
-Button(ventana, text="Sumar").pack(side="left")
-Button(ventana, text="Restar").pack(side="left")
-Button(ventana, text="Multiplicar").pack(side="left")
-Button(ventana, text="Dividir").pack(side="left")
+Button(marco, text="Sumar").pack(side="left", fill=X, expand=YES)
+Button(marco, text="Restar").pack(side="left", fill=X, expand=YES)
+Button(marco, text="Multiplicar").pack(side="left", fill=X, expand=YES)
+Button(marco, text="Dividir").pack(side="left", fill=X, expand=YES)
 
 
 ventana.mainloop()
