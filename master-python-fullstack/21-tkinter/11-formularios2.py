a0077ee5d3562c660d9e6ac80e9b3df0a99437f1
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2020-10-30 23:27:25 +0000

    148. Checkbox

--- master-python-fullstack/21-tkinter/11-formularios2.py
+++ master-python-fullstack/21-tkinter/11-formularios2.py
@@ -12,13 +12,56 @@ encabezado.config(
     bg="green",
     font=("Consolas", 20)
 )
-encabezado.pack(anchor=N, fill=X, expand=YES, side=TOP)
+encabezado.grid(row=0, column=0, columnspan=5, sticky=W)
 
 # Botones check
+web = IntVar()
+movil = IntVar()
 
+def mostrarProfesion():
+    texto = ""
+
+    if web.get():
+        texto += "Desarrollo web"
+
+    if movil.get():
+        if web.get():
+            texto += " y Desarrollo Movil"
+        else:
+            texto += "Desarrollo movil"
+
+    mostrar.config(
+        text=texto,
+        bg="green",
+        fg="white"
+    )
+
+
+Label(ventana, text="¿A que te dedicas?").grid(row=1, column=0)
+Checkbutton(
+    ventana, 
+    text="Desarrollo Web",
+    variable = web,
+    onvalue=1,
+    offvalue=0,
+    command=mostrarProfesion
+).grid(row=2, column=0)
+Checkbutton(
+    ventana, 
+    text="Desarrollo movil",
+    variable=movil,
+    onvalue=1,
+    offvalue=0,
+    command=mostrarProfesion
+).grid(row=3, column=0)
+
+mostrar = Label(ventana)
+mostrar.grid(row=4, column=0)
 
 # Radio buttons
 
 
 # Option Menu
+
+
 ventana.mainloop()
