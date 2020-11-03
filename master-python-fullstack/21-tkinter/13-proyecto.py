d6221a10092ad1e72b91d07812fe664496403136
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2020-11-03 23:14:35 +0000

    154. Menús del programa

--- master-python-fullstack/21-tkinter/13-proyecto.py
+++ master-python-fullstack/21-tkinter/13-proyecto.py
@@ -3,7 +3,7 @@ Crear un programa que tenga:
 - Ventana
 - Tamaño fijo
 - No redimensionable
-- Un menu
+- Un menu (Inicio, Añadir, Información, Salir)
 - Diferentes pantallas
 - Formulario de añadir productos
 - Guardar datos temporalmente
@@ -11,11 +11,21 @@ Crear un programa que tenga:
 """
 from tkinter import *
 
+# Definir ventana
 ventana = Tk()
 ventana.geometry("500x500")
 ventana.title("Proyecto Tkinter - Master en Python")
 ventana.resizable(0, 0)
 
+# Menú superior
+menu_superior = Menu(ventana)
+menu_superior.add_command(label="Inicio")
+menu_superior.add_command(label="Añadir")
+menu_superior.add_command(label="Información")
+menu_superior.add_command(label="Salir", command=(ventana.quit))
 
+# Cargar menú
+ventana.config(menu=menu_superior)
 
+# Cargar ventana
 ventana.mainloop()
