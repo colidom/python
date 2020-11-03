0c7b3f7603b5db508ee85e83f7927a40297a71e6
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2020-11-03 23:10:35 +0000

    153. Ventana, título y geometría

---
+++ master-python-fullstack/21-tkinter/13-proyecto.py
@@ -0,0 +1,21 @@
+"""
+Crear un programa que tenga:
+- Ventana
+- Tamaño fijo
+- No redimensionable
+- Un menu
+- Diferentes pantallas
+- Formulario de añadir productos
+- Guardar datos temporalmente
+- Mos trar datos listados en la pantalla home
+"""
+from tkinter import *
+
+ventana = Tk()
+ventana.geometry("500x500")
+ventana.title("Proyecto Tkinter - Master en Python")
+ventana.resizable(0, 0)
+
+
+
+ventana.mainloop()
