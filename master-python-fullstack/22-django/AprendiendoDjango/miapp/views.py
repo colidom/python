532b31cd1267fff1d7a899b1ad795ff558e56348
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2020-11-25 00:07:56 +0000

    183. Crear vista y rutas

--- master-python-fullstack/22-django/AprendiendoDjango/miapp/views.py
+++ master-python-fullstack/22-django/AprendiendoDjango/miapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+# MVC = Modelo Vista Controlador -> Acciones(métodos)
+# MVT = Modelo Template Vista(vista = controlador) -> Acciones(métodos)
+
+def hola_mundo(request):
+    return HttpResponse("""
+    <h1>Hola Mundo con Django!!</h1>
+    <h3>Soy Carlos Oliva</h3>
+    """)
