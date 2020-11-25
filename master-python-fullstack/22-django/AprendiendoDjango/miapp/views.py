e6775af21aefb4de7775badcfbf4f07441230ef6
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2020-11-25 21:26:52 +0000

    190. Templates separados

--- master-python-fullstack/22-django/AprendiendoDjango/miapp/views.py
+++ master-python-fullstack/22-django/AprendiendoDjango/miapp/views.py
@@ -4,64 +4,18 @@ from django.shortcuts import render, HttpResponse, redirect
 # MVC = Modelo Vista Controlador -> Acciones(métodos)
 # MVT = Modelo Template Vista(vista = controlador) -> Acciones(métodos)
 
-layout = """
-<h1>Sitio web con Django | Carlos Oliva</h1>
-<hr/>
-<ul>
-    <li>
-        <a href="/inicio">Inicio</a>
-    </li>
-    <li>
-        <a href="/hola-mundo">Hola Mundo</a>
-    </li>
-    <li>
-        <a href="/pagina-pruebas">Página pruebas</a>
-    </li>
-    <li>
-        <a href="/contacto">Contacto</a>
-    </li>
-</ul>
-<hr/>
-"""
 def index(request):
+    return render(request, 'index.html', )
 
-    html = """
-        <h1>Inicio</h1>
-        <p>Años hasta el 2050:</p>
-    """
-    year = 2020
-
-    while year <= 2050:
-
-        if year % 2 == 0:
-            html += f"<li>{str(year)}</li>"
-        year +=1
-
-    html += "</ul>"
-    return HttpResponse(layout + html)
 
 def hola_mundo(request):
-    return HttpResponse(layout + """
-        <h1>Hola Mundo con Django!!</h1>
-        <h3>Soy Carlos Oliva</h3>
-    """)
+    return render(request, 'hola_mundo.html')
 
-def pagina(request, redirigir=0):
 
-    if redirigir == 1:
-        return redirect('contacto', nombre="Carlos", apellidos="Oliva")
+def pagina(request):
+    return render(request, 'pagina.html')
 
-    return HttpResponse(layout + """
-        <h1>Página de mi web</h1>
-        <p>Creado por Carlos Oliva</p>
-    """)
 
 def contacto(request, nombre="", apellidos=""):
-    html = ""
-    if nombre and apellidos:
-        html += "<p>El nombre completo es:</p>"
-        html += f"<h3>{nombre} {apellidos}</h3>"
-
-    return HttpResponse(layout + f"<h1>Contacto</h1>" + html)
-        
+    return render(request, 'contacto.html')
     
