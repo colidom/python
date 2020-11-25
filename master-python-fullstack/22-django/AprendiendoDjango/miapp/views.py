0b3e7eb688ad929e0af071bb20890b6e61816248
Carlos Javier Oliva Domínguez <colidom006@gmail.com>
2020-11-25 00:25:08 +0000

    186. Navegación entre rutas

--- master-python-fullstack/22-django/AprendiendoDjango/miapp/views.py
+++ master-python-fullstack/22-django/AprendiendoDjango/miapp/views.py
@@ -4,6 +4,22 @@ from django.shortcuts import render, HttpResponse
 # MVC = Modelo Vista Controlador -> Acciones(métodos)
 # MVT = Modelo Template Vista(vista = controlador) -> Acciones(métodos)
 
+layout = """
+<h1>Sitio web con Django | Carlos Oliva</h1>
+<hr/>
+<ul>
+    <li>
+        <a href="/inicio">Inicio</a>
+    </li>
+    <li>
+        <a href="/hola-mundo">Hola Mundo</a>
+    </li>
+    <li>
+        <a href="/pagina-pruebas">Página pruebas</a>
+    </li>
+</ul>
+<hr/>
+"""
 def index(request):
 
     html = """
@@ -19,16 +35,16 @@ def index(request):
         year +=1
 
     html += "</ul>"
-    return HttpResponse(html)
+    return HttpResponse(layout + html)
 
 def hola_mundo(request):
-    return HttpResponse("""
+    return HttpResponse(layout + """
         <h1>Hola Mundo con Django!!</h1>
         <h3>Soy Carlos Oliva</h3>
     """)
 
 def pagina(request):
-    return HttpResponse("""
+    return HttpResponse(layout + """
         <h1>Página de mi web</h1>
         <p>Creado por Carlos Oliva</p>
     """)
