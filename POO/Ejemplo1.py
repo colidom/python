2443a70d10e32888961ce69ad685f0c16d840b8e
colidom <carlos.oliva.dom@gmail.com>
2021-06-24 00:24:46 +0100

    Cambio de atributos mediante métodos

--- POO/Ejemplo1.py
+++ POO/Ejemplo1.py
@@ -11,8 +11,17 @@ class Humano(): #Creamos la clase Humano
         print(f"Hola soy {self.nombre}, mi edad es {self.edad} y mi ocupación es {self.ocupacion}") #Mensaje
         
 
+    #Creamos un nuevo método para cambiar la ocupación:
+    #En caso que esta persona sea contratada
+        
+    def contratar(self, puesto): #añadimos un nuevo parámetro en el método
+        self.puesto = puesto
+        print (f"{self.nombre} ha sido contratado como {self.puesto}")
+        self.ocupacion = puesto #Ahora cambiamos el atributo ocupación
+
 Persona1 = Humano(31, "Pedro", "Desocupado") #Instancia
 
 #Llamamos al método
-
-Persona1.presentar()
+Persona1.presentar() 
+Persona1.contratar("Obrero")
+Persona1.presentar() #Lo volvemos a presentar luego de su contratación
